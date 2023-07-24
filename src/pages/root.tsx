2abee9e2f7f3a18913d30201cdeb1d9f44c9982d
brokezm <brokesmail@gmail.com>
2023-07-25 00:21:38 +0200

    BA-3: add book search by title

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+
+type Book = {
+  title: string;
+};
+
+function useDebounce(value: unknown, when = 5000) {
+  const [debouncedValue, setDebouncedValue] = useState<unknown>(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, when);
+
+    return () => clearTimeout(timeout);
+  }, [when, value]);
+
+  return debouncedValue || "";
+}
+
 function RootPage() {
-  return <h1>This is the root page.</h1>;
+  const [books, setBooks] = useState<Book[]>([]);
+  const [query, setQuery] = useState<string>("");
+
+  const debouncedQuery = useDebounce(query);
+
+  useEffect(() => {
+    fetch(
+      "https://openlibrary.org/search.json?" +
+        new URLSearchParams({
+          q: debouncedQuery as string,
+        })
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data.docs);
+      })
+      .catch((e) => console.error(e));
+  }, [debouncedQuery]);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      />
+      {books && books.map((book, i) => <h4 key={i}>{book.title}</h4>)}
+    </>
+  );
 }
 
 export default RootPage;
